2d996b3405d69b65b4713c3ac215d3070ab94469
Gandolf_Li <m15572018661@163.com>
2023-04-25 17:47:00 +0800

    5.25 第二次提交

--- Code02/src/class02/Code01_PreSum.java
+++ Code02/src/class02/Code01_PreSum.java
@@ -1,7 +1,52 @@
-package class02;/**
- * @author:  南哥
- * @date:  2023/4/25 15:54
+package class02;
+
+
+/**
+ * @author: 南哥
+ * @date: 2023/4/25 15:54
  * @ClassName: Code01_PreSum
+ * <p>
+ * 把数组中的每一个元素相加，对比RangeSum1和RangeSum2那个算法更好
  */
 public class Code01_PreSum {
+    public static class RangeSum1 {
+        private int[] arr;
+
+        public RangeSum1(int[] array) {
+            arr = array;
+        }
+
+        public int rangeSum(int L, int R) {
+            int sum = 0;
+            for (int i = L; i <= R; i++) {
+                sum += arr[i];
+            }
+            return sum;
+        }
+    }
+
+    public static class RangeSum2 {
+        private int[] preSum;
+
+        public RangeSum2(int[] array) {
+            int N = array.length;
+            preSum = new int[N];       //初始化数组
+            preSum[0] = array[0];      //让preSum[0]置为前缀
+            for (int i = 1; i < N; i++) {
+                preSum[i] = preSum[i - 1] + array[i];      //这段代码的含义是：preSum[i]是preSum的前i个元素加array，for通过遍历整个数组完成preSum的计算。
+            }
+        }
+
+        public int rangeSum(int L, int R) {
+            return L == 0 ? preSum[R] : preSum[R] - preSum[L - 1];  //等于0，就直接返回R值；不等于0，就preSum[R] - preSum[L - 1]，preSum[3]-preSum[1-1] = 17 - 3 : 14
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3, 5, 2, 7, 1};
+        //RangeSum1 rs = new RangeSum1(arr);
+        RangeSum2 rs = new RangeSum2(arr);
+        int sum = rs.rangeSum(1, 3);
+        System.out.println(sum);
+    }
 }
--- Code02/src/class02/Code03_Comp.java
+++ Code02/src/class02/Code03_Comp.java
@@ -1,7 +1,58 @@
-package class02;/**
- * @author:  南哥
- * @date:  2023/4/25 17:18
+package class02;
+
+/**
+ * @author: 南哥
+ * @date: 2023/4/25 17:18
  * @ClassName: Code03_Comp
  */
 public class Code03_Comp {
+
+    public static void selectionSort(int[] arr) {
+        if (arr == null || arr.length < 2) {
+            return;
+        }
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    // 返回一个数组arr，arr长度[0,maxLen - 1]，arr中的每个值[0，maxValue - 1]
+    public static int[] lenRandomValueRandom(int maxLen, int maxValue) {
+        int len = (int) (Math.random() * maxLen);
+        int[] ans = new int[len];
+        for (int i = 0; i < len; i++) {
+            ans[i] = (int) (Math.random() * maxValue);
+        }
+        return ans;
+    }
+
+    public static void printSort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int maxLen = 5;
+        int maxValue = 10;
+        int testTime = 10;
+        for(int i = 0; i < testTime; i++){
+            int[] arr1 = lenRandomValueRandom(maxLen, maxValue);
+            selectionSort(arr1);
+            printSort(arr1);
+        }
+    }
 }
